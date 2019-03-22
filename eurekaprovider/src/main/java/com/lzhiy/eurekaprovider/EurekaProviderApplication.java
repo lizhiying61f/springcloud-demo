3c808826882f5d9b74d6327e6186bd220b8e4d6b
lzhiy <382091679@qq.com>
2019-03-22 15:18:08 +0800

    构建项目

---
+++ eurekaprovider/src/main/java/com/lzhiy/eurekaprovider/EurekaProviderApplication.java
@@ -0,0 +1,44 @@
+package com.lzhiy.eurekaprovider;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+@RestController
+@EnableHystrix
+@EnableHystrixDashboard
+@EnableCircuitBreaker
+public class EurekaProviderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaProviderApplication.class, args);
+    }
+
+    @Value("${server.port}")
+    String port;
+
+    @RequestMapping("/hi")
+    @HystrixCommand(fallbackMethod = "hiError")
+    public String home(@RequestParam(value = "name", defaultValue = "forezp") String name) {
+        RestTemplate restTemplate = new RestTemplate();
+        String rep = restTemplate.getForObject("http://localhost:8765/hi", String.class);
+        return rep +  "hi " + name + " ,i am from port:" + port;
+    }
+
+    public String hiError(String name){
+        return "this is client providerhi" + name + "error!";
+    }
+}
